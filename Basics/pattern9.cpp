0f730bbb9783394a2c6cdec53c19fba8bbc466c6
Tanyarajput677 <tr0049793@gmail.com>
2026-07-15 00:40:56 +0530

    Add pattern9 function for star pattern printing
    Implement pattern9 function to print a specific star pattern based on input size.

---
+++ Basics/pattern9.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    void pattern9(int n) {
+        for(int i=0;i<n;i++){
+            for(int j=0;j< n-i-1;j++){
+                cout<<" ";
+            }
+            for(int k=0;k< 2*i+1;k++){
+                cout<<"*";
+            }
+            cout<<endl;
+        }
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<i;j++){
+                cout<<" ";
+            }
+            for(int k=0;k<2*(n-i)-1;k++){
+                cout<<"*";
+            }
+            cout<<endl;
+        }
+
+
+    }
+};
