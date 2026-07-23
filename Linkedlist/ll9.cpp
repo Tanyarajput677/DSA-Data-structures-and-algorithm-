eb71a81cb669cfa23e32b8643ccbd6a1efa57135
Tanyarajput677 <tr0049793@gmail.com>
2026-07-23 21:51:02 +0530

    Implement cycle detection in linked list

---
+++ Linkedlist/ll9.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        ListNode* slow=head;
+        ListNode* fast=head;
+        while(fast!=NULL && fast->next!=NULL){
+            slow=slow->next;
+            fast=fast->next->next;
+            if(slow==fast){
+                ListNode* entry=head;
+                while(slow!=entry){
+                    entry=entry->next;
+                    slow=slow->next;
+                }
+                return entry;
+            }
+            
+        }
+        return NULL;
+
+    }
+};
