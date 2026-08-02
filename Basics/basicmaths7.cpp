0a0e2bb4241de6d1d7639866e625a97c939750be
Tanyarajput677 <tr0049793@gmail.com>
2026-08-02 16:33:00 +0530

    Implement Armstrong number checker in basicmaths7.cpp

---
+++ Basics/basicmaths7.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isArmstrong(int n) {
+        int original=n;
+        int digits=0;
+        int temp=n;
+        while(temp > 0){
+            digits++;
+            temp/=10;
+        }
+        int sum=0;
+        temp=n;
+        while(temp>0){
+            int digit=temp%10;
+            sum+=pow(digit,digits);
+            temp/=10;
+        }
+        return sum==original;
+
+    }
+};
