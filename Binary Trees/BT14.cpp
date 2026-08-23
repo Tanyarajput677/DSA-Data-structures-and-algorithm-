776234139bd076557371f52016c187e54d4307ad
Tanyarajput677 <tr0049793@gmail.com>
2026-08-23 12:46:11 +0530

    Add maxPath and maxPathSum methods for binary tree
    Implement maxPath and maxPathSum functions to calculate the maximum path sum in a binary tree.

---
+++ Binary Trees/BT14.cpp
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int maxi=INT_MIN;
+    int maxPath(TreeNode* root) {
+        if(root==NULL){
+            return 0;
+        }
+        int lh=max(0,maxPath(root->left));
+        int rh=max(0,maxPath(root->right));
+        maxi=max(maxi,root->val+lh+rh);
+        return root->val+ max(lh,rh);
+    }
+    int maxPathSum(TreeNode* root){
+        maxPath(root);
+        return maxi;
+    }
+};
