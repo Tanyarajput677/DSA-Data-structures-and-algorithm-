62b5e14be11c9daed4337cbc50df2875473419c5
Tanyarajput677 <tr0049793@gmail.com>
2026-08-29 21:48:49 +0530

    Add bipartite graph check using DFS
    Implement DFS to check if a graph is bipartite.

---
+++ Graphs/g11.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool dfs(int node,vector<vector<int>>& graph,vector<int>& color){
+        for(int neighbour:graph[node]){
+            if(color[neighbour]==-1){
+                color[neighbour]=1-color[node];
+                if(!dfs(neighbour,graph,color)){
+                    return false;
+                }
+            }
+            else if(color[neighbour]==color[node]){
+                return false;
+            }
+        }
+        return true;
+
+    }
+    bool isBipartite(vector<vector<int>>& graph) {
+        int n=graph.size();
+        vector<int> color(n,-1);
+        for(int i=0;i<n;i++){
+            if(color[i]==-1){
+                color[i]=0;
+                if(!dfs(i,graph,color)){
+                    return false;
+                }
+            }
+        }
+        return true;
+        
+    }
+};
