9c2dd755caacc4b613bb15df78397d6413923580
Tanyarajput677 <tr0049793@gmail.com>
2026-08-30 17:05:27 +0530

    Implement course scheduling solution in g13.cpp

---
+++ Graphs/g13.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<vector<int>> adj(numCourses);
+        for(auto it:prerequisites){
+            int course=it[0];
+            int prerequisite=it[1];
+            adj[prerequisite].push_back(course);
+        }
+
+        vector<int> indegree(numCourses,0);
+        for(int i=0;i<numCourses;i++){
+            for(int neighbour:adj[i]){
+                indegree[neighbour]++;
+            }
+        }
+        queue<int> q;
+        for(int i=0;i<numCourses;i++){
+            if(indegree[i]==0){
+                q.push(i);
+            }
+        }
+        int count=0;
+        while(!q.empty()){
+            int node=q.front();
+            q.pop();
+
+            count++;
+            for(int neighbour:adj[node]){
+                indegree[neighbour]--;
+                if(indegree[neighbour]==0){
+                    q.push(neighbour);
+                }
+            }
+        }
+        return count==numCourses;
+    }
+};
