3b91cdda3ed670fdef457bac60a4825b458f7828
Tanyarajput677 <tr0049793@gmail.com>
2026-09-09 23:57:22 +0530

    Implement findTarget function in BST class

---
+++ Binary Search Tree/BST10.cpp
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool findTarget(TreeNode* root, int k) {
+        unordered_set<int> st;
+        return solve(root,k,st);
+    }
+    bool solve(TreeNode* root,int k,unordered_set<int>& st){
+        if(root==nullptr){
+            return false;
+        }
+        if(st.find(k-root->val)!=st.end()){
+            return true;
+        }
+        st.insert(root->val);
+        return solve(root->left,k,st) || solve(root->right,k,st);
+    }
+};
